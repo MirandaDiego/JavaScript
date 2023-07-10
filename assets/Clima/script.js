1e355f62be17c83ba1e04a3df45d0fcddfce49e4
MirandaDiego <diegoflam12@gmail.com>
2023-07-10 17:19:44 -0300

    atualizando

---
+++ assets/Clima/script.js
@@ -0,0 +1,57 @@
+//prevenindo o comportamento padrao do input:
+
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if(input !== ''){
+        clearInfo();
+        showWarning('carregando...')
+        
+        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURI(input)}&appid=2208503ceec5aac86d0af52c0c941cf4&units=metric&lang=pt_br`
+        
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod ===200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        }else{
+            clearInfo();
+            showWarning('Não encontramos essa localização!');
+        }
+    }else{
+        clearInfo();
+    }
+
+});
+
+function showInfo(json){
+    showWarning('');
+
+    document.querySelector('.resultado').style.display = 'block';
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle -90}deg)`
+}
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none'
+}
+
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg;
+}
+
